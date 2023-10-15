06e700a55af17905de7bb54d451e8463736f7864
Lauriethefish <lauriethefish@outlook.com>
2023-10-15 17:23:33 +0100

    Change schema version to 1.1.0 since features added

--- QuestPatcher.QMod/QModManifest.cs
+++ QuestPatcher.QMod/QModManifest.cs
@@ -156,7 +156,7 @@ namespace QuestPatcher.QMod
             "0.1.1",
             "0.1.2",
             "1.0.0",
-            "1.0.1"
+            "1.1.0"
         }.ToHashSet();
 
         private const string LatestSchemaVersion = "1.0.0";
